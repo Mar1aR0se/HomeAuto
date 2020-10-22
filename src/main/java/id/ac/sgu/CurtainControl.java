e91f7725f784a9f0b5e88248a1756e08b3f9fbf9
Christoph Hümbert <christoph.huembert@commsult.de>
2020-10-22 11:39:20 +0200

    update project to template

---
+++ src/main/java/id/ac/sgu/CurtainControl.java
@@ -0,0 +1,13 @@
+package id.ac.sgu;
+
+public class CurtainControl {
+	private boolean cover;
+
+	public boolean isCover() {
+		return cover;
+	}
+
+	public void setCover(boolean cover) {
+		this.cover = cover;
+	}
+}
---
+++ src/main/java/id/ac/sgu/MediaElectronic.java
@@ -0,0 +1,14 @@
+package id.ac.sgu;
+
+public class MediaElectronic {
+	private int volume;
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+	
+}
